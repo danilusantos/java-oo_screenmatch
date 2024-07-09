a44d876c4a204b060798f8a5c4e2d5c9b095143d
Danilo Santos <danilondosantos@gmail.com>
2024-07-08 21:50:07 -0300

    feat: :sparkles: defining actions for the film

--- src/Filme.java
+++ src/Filme.java
@@ -1,6 +1,26 @@
-public class Filme {
+public class Filme
+{
     String nome;
-    int anoDeLancamento, totalDeAvaliacoes, duracaoEmMinutos;
+    int anoDeLancamento;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
+
+    void exibeFichaTecnica ()
+    {
+        System.out.println("Filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia (double nota)
+    {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMediaDasAvaliacoes ()
+    {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,18 @@
-public class Principal {
-    public static void main(String[] args) {
+public class Principal
+{
+    public static void main(String[] args)
+    {
         Filme filme = new Filme();
         filme.nome = "Top Gun: Maverick";
         filme.anoDeLancamento = 2022;
-        filme.avaliacao = 8.6;
-        filme.incluidoNoPlano = true;
-        filme.totalDeAvaliacoes = 30;
         filme.duracaoEmMinutos = 189;
 
-        System.out.println(filme.nome);
-        System.out.printf("%s minutos.", filme.duracaoEmMinutos);
+        filme.exibeFichaTecnica();
+        filme.avalia(8);
+        filme.avalia(5);
+        filme.avalia(10);
+        System.out.println(filme.somaDasAvaliacoes);
+        System.out.println(filme.totalDeAvaliacoes);
+        System.out.printf("%.2f", filme.pegaMediaDasAvaliacoes());
     }
 }
